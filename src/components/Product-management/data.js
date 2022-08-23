fcad517423e5216a6f3843b64cfd8033448d0c2d
ChanduniNethmini <nchanduni21@gmail.com>
2022-08-24 01:29:21 +0530

    added product adding and cart

---
+++ src/components/Product-management/data.js
@@ -0,0 +1,36 @@
+import img1 from '../../images/img1.jpg';
+import img2 from '../../images/img2.jpg';
+import img3 from '../../images/img3.jpg';
+
+
+
+const data={
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: 'Innovex DDR195 180L Double Door Direct Cool Refrigerator',
+            desc:'',
+            price:120000,
+
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Kenwood Citrus Juicer JE280A - 1',
+            desc:'',
+            price:20000,
+
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'NIKAI Air Fryer NAF779A - 7L Capacity',
+            desc:'',
+            price:50000,
+
+        },
+    ],
+};
+
+export default data;
