cd6929542f3cc9c5c15ec82254241e6d56a08e00
Avdunusinghe <avdunusinghe@gmail.com>
2022-09-03 23:04:46 +0530

    contact us form implemented

--- src/components/Contact-Us/Contact-Us.jsx
+++ src/components/Contact-Us/Contact-Us.jsx
@@ -4,24 +4,36 @@ import { blue } from "@mui/material/colors";
 import "./Contact-Us.css";
 import { InputText } from "primereact/inputtext";
 import { classNames } from "primereact/utils";
+import { InputTextarea } from "primereact/inputtextarea";
 
 const ContactUs = () => {
 	return (
 		<div class="contact-row">
 			<div class="col-md-12">
-				<form action="index.html" method="post">
+				<form>
 					<h2> Contact us </h2>
 					<div className="container">
-						<div className="field col">
-							<span className="p-float-label">
-								<InputText classNames="txtInput" className="txtInput" id="email" required />
-								<label htmlFor="email">Email </label>
-							</span>
+						<div>
+							<InputText classNames="txtInput" className="txtInput" id="email" required placeholder="Name" />
 						</div>
-						<span className="p-float-label">
-							<InputText classNames="txtInput" className="txtInput" id="email" required />
-							<label htmlFor="email">Email </label>
-						</span>
+						<div>
+							<InputText classNames="txtInput" className="txtInput" id="email" required placeholder="Email" />
+						</div>
+						<div>
+							<InputText classNames="txtInput" className="txtInput" id="email" required placeholder="Mobile Number" />
+						</div>
+						<div>
+							<InputTextarea
+								classNames="txtInput"
+								className="txtInput"
+								rows={5}
+								cols={30}
+								autoResize
+								placeholder="Message"
+							/>
+						</div>
+
+						<Button classNames="txtInput" className="txtInput" label="Submit" aria-label="Submit" />
 					</div>
 				</form>
 			</div>
