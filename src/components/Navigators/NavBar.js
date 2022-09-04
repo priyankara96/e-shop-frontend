8cd99fa891f85be7e90c83e5b9f19fa1c9ba49dc
Priyankara Athapaththu <chamidupriyankara.sl@gmail.com>
2022-09-04 10:25:31 +0530

    styles updated

--- src/components/Navigators/NavBar.js
+++ src/components/Navigators/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { AiOutlineLogin } from "react-icons/ai";
 
 export class NavBar extends Component {
   render() {
@@ -34,6 +35,11 @@ export class NavBar extends Component {
                 <a class="nav-link disabled">User Profile</a>
               </div>
             </div>
+            <form class="form-inline my-2 my-lg-0">
+            <a class="nav-link" href="/login">
+              <button type="button" className="btn btn-light"  style={{ textDecoration: 'none', color: 'Info' }}><AiOutlineLogin />&nbsp; Sign in</button>
+            </a>
+            </form>
           </div>
         </nav>
       </div>
