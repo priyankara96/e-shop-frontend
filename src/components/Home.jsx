353fca0ba142f8801b2dc641b2d9cc9b31de7969
ChanduniNethmini <nchanduni21@gmail.com>
2022-09-06 01:07:47 +0530

    modified home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import HomeCart from "./Product-management/Home";
+import Slider from "./Product-management/Slider";
 
 export default class Home extends Component {
 	render() {
 		return (
 			<div>
+<Slider/>
 				<HomeCart />
 				{/* <h1>e-shop-frontend</h1> */}
+				
 			</div>
 		);
 	}
