639075f9f37a2ba15427d3744ffb79bdba620033
ChanduniNethmini <nchanduni21@gmail.com>
2022-09-13 23:54:42 +0530

    Modified Shopping Cart

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,13 @@ export default class Home extends Component {
 		return (
 			<div>
 <Slider/>
-				<HomeCart />
+
+<br/><hr/>
+<br/>
+<div className="text-center">
+<h1><font face = "Comic sans MS" size =" 6">Our Products</font></h1></div>
+<br/><br/>
+				<HomeCart /> <br/><br/>
 				{/* <h1>e-shop-frontend</h1> */}
 				
 			</div>
